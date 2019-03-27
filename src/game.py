1865479c700f9e2d39237e797efaebad7fc31486
CoffeeNails <jonathan.leinola@aalto.fi>
2019-03-27 22:19:53 +0200

    lisätään main looppi, initalisoidaan mikseri ja screeni

--- src/game.py
+++ src/game.py
@@ -1,1 +1,28 @@
 import pygame
+import pygame.freetype
+import constants
+from spritesheet import SpriteSheet
+import random
+
+def main():
+    #p‰‰ohjelma
+    pygame.init()
+    
+    #initalisoidaan mixeri
+    pygame.mixer.init()
+    mixer=pygame.mixer.music
+    filename="sound.wav"
+    mixer.load(filename)
+    
+    #Asetetaan screenin leveys ja korkeus
+    size=[constants.SCREEN_WIDTH,constants.SCREEN_HEIGHT]
+    screen=pygame.display.set_mode(size)
+    
+    #Asetetaan fontti
+    GAME_FONT=pygame.freetype.Font("font.ttf",50)
+    pygame.display.set_caption("Father Long Legs")
+    
+    
+    
+if __name__ == "__main__":
+    main()
