9dcd4830dff31e39a791439a1e64bc8a82fc81c9
CoffeeNails <jonathan.leinola@aalto.fi>
2019-03-27 22:19:22 +0200

    lisätään muuttujat

---
+++ src/constants.py
@@ -0,0 +1,11 @@
+#Globaalit muuttujat
+#Asetetaan värit
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+ 
+#Näytön resoluutio
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
