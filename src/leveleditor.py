714eda13f61edab7b53e3792c8217f3ba1eefc71
CoffeeNails <jonathan.leinola@aalto.fi>
2019-04-20 22:35:07 +0300

    leveleditorissa voi nyt ladata vanhan levelin myös ja muokata sitä

--- src/leveleditor.py
+++ src/leveleditor.py
@@ -1,8 +1,9 @@
 import pygame
 import re
 import constants
+from game import Platform
 pygame.init()
-file = open('levels/level.txt', 'w')
+file = open('levels/level.txt', 'a+')
 size = width, height = 2000,800
 window = pygame.display.set_mode(size)
 pygame.display.set_caption( "leveleditor" )
@@ -10,9 +11,26 @@ pygame.display.set_caption( "leveleditor" )
 clock = pygame.time.Clock()
 fps = 60
 
+GAME_FONT = pygame.freetype.Font("font.ttf", 50)
 to_draw = []
 current_color = 0
 draw_start = False
+
+def splitlevel(filename):
+    level = []
+    with open(filename) as f:
+        for item in f:
+            level.append([int(i) for i in item.split()])
+    f.close
+    return level
+
+level = splitlevel('levels/level.txt')
+ 
+for platform in level:
+    block = Platform(platform[0], platform[1])
+    block.rect.x = platform[2]
+    block.rect.y = platform[3]
+    to_draw.append(block)
 
 
 
@@ -60,7 +78,10 @@ while running:
         pygame.draw.rect(window,constants.BLACK, pygame.Rect(pos, (mouse_pos[0] - pos[0],mouse_pos[1]- pos[1])))
     for item in to_draw:
         pygame.draw.rect(window,constants.BLACK,item)
-
+    
+    
+    GAME_FONT.render_to(window, (0, 760),"x: "+str(mouse_pos[0])+" y: "+str(mouse_pos[1]), constants.GREEN)
+           
 
 
     pygame.display.update()
